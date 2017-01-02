ffba7a801665013d9854524515298e259bddcc3e
Nick Luo <nick.luo@me.com>
2017-01-02 17:50:17 +1000

    Update navigation bar. Remove jquery dependency

--- app/components/layouts/navigation.tsx
+++ app/components/layouts/navigation.tsx
@@ -1,43 +1,13 @@
 import * as React from 'react'
+import AppBar from 'material-ui/AppBar'
 
 export function Navigation () {
     return (
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1">
-                  <span className="sr-only">Toggle navigation</span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                  <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="http://disputebills.com">
-                    <img
-                        src="http://res.cloudinary.com/candidbusiness/image/upload/v1455406304/dispute-bills-chicago.png"
-                        alt="Dispute Bills"
-                    /> 
-                </a>
-              </div>
-              <div id="navbar1" className="navbar-collapse collapse">
-                <ul className="nav navbar-nav">
-                  <li className="active"><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Contact</a></li>
-                  <li className="dropdown">
-                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                    <ul className="dropdown-menu" role="menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
-                      <li className="divider"></li>
-                      <li className="dropdown-header">Nav header</li>
-                      <li><a href="#">Separated link</a></li>
-                      <li><a href="#">One more separated link</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+        <nav>
+          <AppBar
+            title="My Diaries List"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+        </nav>
     )
 }
