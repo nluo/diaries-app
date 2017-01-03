1fe77f66ad4aed388bab10b43647096937eab324
Nick Luo <nick.luo@me.com>
2017-01-03 13:14:45 +1000

    Update to es5 promise syntax

--- app/api/diary.ts
+++ app/api/diary.ts
@@ -9,22 +9,20 @@ interface DiaryEntry {
     author?: string
 }
 
-export async function getAll() {
-    try {
-        const diaries = (await axios.get('http://localhost:8000/diaryentries/')).data
-        store.dispatch(getDiariesSuccess(diaries))
-    } catch (error) {
+export function getAll() {
+    return axios.get('http://localhost:8000/diaryentries/').then((response: any) => {
+        store.dispatch(getDiariesSuccess(response.diaries))
+    }).catch((error) => {
         console.log('get all diaryies error is ', error)
-    }
+    })
 }
 
-export async function get(id: number) {
-    try {
-        const diary = (await axios.get(`http://localhost:8000/diaryentries/${id}`)).data
-        store.dispatch(getDiarySuccess(diary))
-    } catch (error) {
-
-    }
+export function get(id: number) {
+    return axios.get(`http://localhost:8000/diaryentries/${id}`).then((response: any) => {
+        store.dispatch(getDiariesSuccess(response.diaries))
+    }).catch((error) => {
+        console.log('get all diaryies error is ', error)
+    })
 }
 
 export function create (payload: DiaryEntry) {
