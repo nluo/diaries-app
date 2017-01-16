abb9c256ce6220d86a17492a0db0f24bb882e527
Nick Luo <nick.luo@me.com>
2017-01-16 17:44:00 +1000

    Refactor isAuthenticated component to be stateless

--- app/components/hoc/authenticated.tsx
+++ app/components/hoc/authenticated.tsx
@@ -3,19 +3,16 @@ const cookie = require('react-cookie')
 import * as constants from '../../constants'
 
 export function isAuthenticated(Component: any) {
-    class AuthentcationWrap extends React.Component<any, any> {
-        render() {
-            // check cookies
-            let authenticated = cookie.load(constants.AUTHENTICATION_COOKIE)
-            if (authenticated) {
-                let email: string = cookie.load(constants.COOKIE_USER_EMAIL)
-                const newProps: any = {
-                    userEmail: email
-                }
-                return (<Component {...this.props} {...newProps} />)
+    return function (props: any) {
+        // check cookies
+        let authenticated = cookie.load(constants.AUTHENTICATION_COOKIE)
+        if (authenticated) {
+            let email: string = cookie.load(constants.COOKIE_USER_EMAIL)
+            const newProps: any = {
+                userEmail: email
             }
-            return null
+            return (<Component {...props} {...newProps} />)
         }
+        return null
     }
-    return AuthentcationWrap
 }
